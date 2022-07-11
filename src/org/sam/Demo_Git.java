397984cee209c45b12b1593b7567e67e36231c64
shainsha04 <shainsha4896@gmail.com>
2022-07-11 18:03:25 +0530

    New project updated

--- src/org/sam/Demo_Git.java
+++ src/org/sam/Demo_Git.java
@@ -5,11 +5,21 @@ public class Demo_Git {
 	public Demo_Git() {
 		System.out.println("shainsha demo");
 	}
+
 	private void Demo_Update() {
 		System.out.println("Update checking");
 	}
+
 	private void Demo_Owncheck() {
 		System.out.println("Checking own");
 	}
-}
 
+	private void MasterUpdate() {
+		System.out.println("Checked and updated");
+	}
+
+	private void completed() {
+		System.out.println("Worked Completed");
+	}
+
+}
